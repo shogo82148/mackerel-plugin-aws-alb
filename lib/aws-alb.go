daed037a3b4d90737fd29e2b002487e6c497a2e2
Ichinose Shogo <shogo82148@gmail.com>
2018-03-07 21:20:50 +0900

    support target groups

--- lib/aws-alb.go
+++ lib/aws-alb.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"flag"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,20 +15,6 @@ import (
 	mp "github.com/mackerelio/go-mackerel-plugin"
 )
 
-var graphdef = map[string]mp.Graphs{
-	"alb.response_ext": {
-		Label: "Response Time Percentile",
-		Unit:  "float",
-		Metrics: []mp.Metrics{
-			{Name: "p99", Label: "p99"},
-			{Name: "p95", Label: "p95"},
-			{Name: "p90", Label: "p90"},
-			{Name: "p50", Label: "p50"},
-			{Name: "p10", Label: "p10"},
-		},
-	},
-}
-
 // Plugin is ALB plugin for mackerel.
 type Plugin struct {
 	Region          string
@@ -35,6 +22,8 @@ type Plugin struct {
 	SecretAccessKey string
 	CloudWatch      *cloudwatch.CloudWatch
 	LBName          string
+	TargetGroups    []string
+	Prefix          string
 }
 
 func (p *Plugin) prepare() error {
@@ -53,10 +42,45 @@ func (p *Plugin) prepare() error {
 
 	p.CloudWatch = cloudwatch.New(sess, config)
 
+	dimensions := []*cloudwatch.DimensionFilter{
+		{
+			Name: aws.String("TargetGroup"),
+		},
+	}
+	if p.LBName != "" {
+		dimensions = append(dimensions, &cloudwatch.DimensionFilter{
+			Name:  aws.String("LoadBalancer"),
+			Value: aws.String(p.LBName),
+		})
+	}
+	ret, err := p.CloudWatch.ListMetrics(&cloudwatch.ListMetricsInput{
+		Namespace:  aws.String("AWS/ApplicationELB"),
+		Dimensions: dimensions,
+		MetricName: aws.String("TargetResponseTime"),
+	})
+	if err != nil {
+		return err
+	}
+	p.TargetGroups = make([]string, 0, len(ret.Metrics))
+	for _, met := range ret.Metrics {
+		if len(met.Dimensions) != 2 {
+			continue
+		}
+		for _, d := range met.Dimensions {
+			if name := d.Name; name != nil && *name != "TargetGroup" {
+				continue
+			}
+			if d.Value == nil {
+				continue
+			}
+			p.TargetGroups = append(p.TargetGroups, *d.Value)
+		}
+	}
+
 	return nil
 }
 
-func (p Plugin) getLastPercentile(stat map[string]float64, dimensions []*cloudwatch.Dimension, metricName string) error {
+func (p *Plugin) getLastPercentile(stat map[string]float64, prefix string, dimensions []*cloudwatch.Dimension, metricName string) error {
 	now := time.Now()
 
 	response, err := p.CloudWatch.GetMetricStatistics(&cloudwatch.GetMetricStatisticsInput{
@@ -90,15 +114,41 @@ func (p Plugin) getLastPercentile(stat map[string]float64, dimensions []*cloudwa
 			latest = dp.Timestamp
 			latestVal = *dp.ExtendedStatistics[percentile]
 		}
-		stat[percentile] = latestVal
+		stat[prefix+percentile] = latestVal
 	}
 
 	return nil
 }
 
+// MetricKeyPrefix returns key prefix
+func (p *Plugin) MetricKeyPrefix() string {
+	return p.Prefix
+}
+
 // FetchMetrics fetch elb metrics
-func (p Plugin) FetchMetrics() (map[string]float64, error) {
+func (p *Plugin) FetchMetrics() (map[string]float64, error) {
 	stat := make(map[string]float64)
+
+	for _, tg := range p.TargetGroups {
+		glb := []*cloudwatch.Dimension{
+			{
+				Name:  aws.String("TargetGroup"),
+				Value: aws.String(tg),
+			},
+		}
+		if p.LBName != "" {
+			g2 := &cloudwatch.Dimension{
+				Name:  aws.String("LoadBalancer"),
+				Value: aws.String(p.LBName),
+			}
+			glb = append(glb, g2)
+		}
+		name := strings.Split(tg, "/")
+
+		if err := p.getLastPercentile(stat, "response_ext_per_group."+name[1]+".", glb, "TargetResponseTime"); err != nil {
+			return nil, err
+		}
+	}
 
 	glb := []*cloudwatch.Dimension{}
 	if p.LBName != "" {
@@ -109,7 +159,7 @@ func (p Plugin) FetchMetrics() (map[string]float64, error) {
 		glb = append(glb, g2)
 	}
 
-	if err := p.getLastPercentile(stat, glb, "TargetResponseTime"); err != nil {
+	if err := p.getLastPercentile(stat, "", glb, "TargetResponseTime"); err != nil {
 		return nil, err
 	}
 
@@ -117,7 +167,43 @@ func (p Plugin) FetchMetrics() (map[string]float64, error) {
 }
 
 // GraphDefinition for Mackerel
-func (p Plugin) GraphDefinition() map[string]mp.Graphs {
+func (p *Plugin) GraphDefinition() map[string]mp.Graphs {
+	/*met := make([]mp.Metrics, 0, 5*len(p.TargetGroups))
+	for _, tg := range p.TargetGroups {
+		name := strings.Split(tg, "/")
+		for _, percentile := range [...]string{"p99", "p95", "p90", "p50", "p10"} {
+			met = append(met, mp.Metrics{
+				Name:  name[1] + "_" + percentile,
+				Label: percentile,
+			})
+		}
+	}*/
+
+	graphdef := map[string]mp.Graphs{
+		"response_ext": {
+			Label: "Response Time Percentile",
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "p99", Label: "p99"},
+				{Name: "p95", Label: "p95"},
+				{Name: "p90", Label: "p90"},
+				{Name: "p50", Label: "p50"},
+				{Name: "p10", Label: "p10"},
+			},
+		},
+		"response_ext_per_group.#": {
+			Label: "Response Time Percentile per Target Group",
+			Unit:  mp.UnitFloat,
+			Metrics: []mp.Metrics{
+				{Name: "p99", Label: "p99"},
+				{Name: "p95", Label: "p95"},
+				{Name: "p90", Label: "p90"},
+				{Name: "p50", Label: "p50"},
+				{Name: "p10", Label: "p10"},
+			},
+		},
+	}
+
 	return graphdef
 }
 
@@ -128,6 +214,7 @@ func Do() {
 	optAccessKeyID := flag.String("access-key-id", "", "AWS Access Key ID")
 	optSecretAccessKey := flag.String("secret-access-key", "", "AWS Secret Access Key")
 	optTempfile := flag.String("tempfile", "", "Temp file name")
+	optPrefix := flag.String("metric-key-prefix", "alb", "Metric key prefix")
 	flag.Parse()
 
 	var alb Plugin
@@ -143,13 +230,14 @@ func Do() {
 	alb.AccessKeyID = *optAccessKeyID
 	alb.SecretAccessKey = *optSecretAccessKey
 	alb.LBName = *optLBName
+	alb.Prefix = *optPrefix
 
 	err := alb.prepare()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	helper := mp.NewMackerelPlugin(alb)
+	helper := mp.NewMackerelPlugin(&alb)
 	helper.Tempfile = *optTempfile
 
 	helper.Run()
